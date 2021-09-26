106fe1610377745b92babca876f69728d5600111
lushcodes <lushcodes@gmail.com>
2021-09-26 14:29:02 +0100

    add navbar logo

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import logo from '../../assests/logo.png';
+
+const Navbar = () => {
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="Blitz VST"
+              height="25px"
+              className={classes.image}
+            />
+            Blitz VST
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+
+export default Navbar;
