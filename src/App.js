04be667389a526d6587086c884e290cfd8560394
lushcodes <lushcodes@gmail.com>
2021-09-26 22:21:55 +0100

    add cart

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import { Products, Navbar } from './components';
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
-
     setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    const cart = await commerce.cart.retrieve();
+    setCart(cart);
   };
 
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
 
-  console.log(products);
+  console.log(cart);
 
   return (
     <div>
