95381ee8d8da0900fb3b1fe482b079744e838f63
lushcodes <lushcodes@gmail.com>
2021-09-27 15:55:05 +0100

    implement routes and button logic

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import CartItem from './CartItem/CartItem';
-
+import { Link } from 'react-router-dom';
 import useStyles from './styles';
 
-const Cart = ({ cart }) => {
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
   const classes = useStyles();
 
   const EmptyCart = () => {
     return (
       <Typography variant="subtitle1">
         You have no items in your cart!
+        <Link to="/" className={classes.link}>
+          Go back to the shop
+        </Link>
       </Typography>
     );
   };
@@ -21,7 +29,11 @@ const Cart = ({ cart }) => {
         <Grid container spacing={3}>
           {cart.line_items.map((item) => (
             <Grid item xs={12} sm={4} key={item.id}>
-              <CartItem item={item} />
+              <CartItem
+                item={item}
+                onUpdateCartQty={handleUpdateCartQty}
+                onRemoveFromCart={handleRemoveFromCart}
+              />
             </Grid>
           ))}
         </Grid>
@@ -36,6 +48,7 @@ const Cart = ({ cart }) => {
               type="button"
               variant="contained"
               color="secondary"
+              onClick={handleEmptyCart}
             >
               Empty Cart
             </Button>
