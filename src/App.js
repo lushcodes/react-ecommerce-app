4cbba9b0c8a65d0a114c5b7e2240ec670266f008
lushcodes <lushcodes@gmail.com>
2021-09-27 12:55:13 +0100

    add items to cart functionality

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ const App = () => {
     setCart(cart);
   };
 
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item.cart);
+  };
+
   useEffect(() => {
     fetchProducts();
     fetchCart();
@@ -25,8 +30,8 @@ const App = () => {
 
   return (
     <div>
-      <Navbar />
-      <Products products={products} />
+      <Navbar totalItems={cart.total_items} />
+      <Products products={products} onAddToCart={handleAddToCart} />
     </div>
   );
 };
